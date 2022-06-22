2e667e6d5b510daae5ff072033005e75d85fef5c
Yeonsuk Yoo <yysuk0302@gmail.com>
2022-06-22 12:33:14 -0400

    feat: Update _app.tsx for SEO and add open graph

--- src/frontend/components/ui/Header.tsx
+++ src/frontend/components/ui/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
     <>
       <header className="sticky top-0 z-[1] bg-white px-4 py-2 shadow-md">
         <div className="mx-auto flex w-full max-w-sm items-center justify-between">
-          <h1 className="text-xl font-semibold text-gray-800">Image Renamer</h1>
+          <h1 className="text-xl font-semibold text-gray-800">Re:ceipt</h1>
           <div className="flex items-center space-x-1.5 text-gray-700">
             <span>{user}</span>
             <button onClick={() => setShowModal(true)}>
