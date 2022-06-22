215bc04ee31f6a3e5bf02058eca0eb1f203ab0b1
Yeonsuk Yoo <yysuk0302@gmail.com>
2022-06-22 00:44:58 -0400

    fix: Erase securityHeader in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,38 +2,31 @@
 
 const { StatusCodes } = require('http-status-codes');
 
-const securityHeaders = [
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block',
-  },
-  {
-    key: 'Content-Security-Policy',
-    value: [
-      `default-src 'self'`,
-      `base-uri 'self'`,
-      `style-src 'self' fonts.googleapis.com 'unsafe-inline'`,
-      `font-src 'self' fonts.gstatic.com`,
-      `script-src 'self' 'unsafe-eval' 'unsafe-inline'`,
-      `object-src 'self'`,
-      `img-src 'self' blob: data:;`,
-      `worker-src 'self' blob:;`,
-    ].join(';'),
-  },
-];
+// const securityHeaders = [
+//   {
+//     key: 'X-XSS-Protection',
+//     value: '1; mode=block',
+//   },
+//   {
+//     key: 'Content-Security-Policy',
+//     value: [
+//       `default-src 'self'`,
+//       `base-uri 'self'`,
+//       `style-src 'self' fonts.googleapis.com 'unsafe-inline'`,
+//       `font-src 'self' fonts.gstatic.com`,
+//       `script-src 'self' 'unsafe-eval' 'unsafe-inline'`,
+//       `object-src 'self'`,
+//       `img-src 'self' blob: data:;`,
+//       `worker-src 'self' blob:;`,
+//       ,
+//     ].join(';'),
+//   },
+// ];
 
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
   async redirects() {
     return [
       {
