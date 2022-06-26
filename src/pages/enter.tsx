f4170ce164a4fbfab4927982c22ac55b4ced4dbf
Yeonsuk Yoo <yysuk0302@gmail.com>
2022-06-27 01:20:44 +0900

    feat: Add Coxwave logo and handle error on enter page

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,11 +1,13 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
+import Logo from '@frontend/components/core/Logo';
 import { EnterLayout } from '@frontend/components/layout';
 import { Button, Input } from '@frontend/components/ui';
 import { useUser } from '@frontend/hooks/use-user';
 
 export default function EnterPage() {
   const [name, setName] = useState('');
+  const [error, setError] = useState(false);
 
   const { mutate } = useUser();
 
@@ -14,13 +16,33 @@ export default function EnterPage() {
     mutate();
   }, [name, mutate]);
 
+  useEffect(() => {
+    if (name.length > 4) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  }, [name]);
+
   return (
-    <div className="flex h-full min-h-screen w-full flex-col justify-center bg-cox-deep-blue-900 px-4">
+    <div className="relative flex h-full min-h-screen w-full flex-col justify-center px-4">
+      <div className="absolute top-32 left-1/2 -translate-x-1/2 space-y-2 text-center">
+        <Logo className="w-48" />
+        <h1 className="text-4xl font-bold">Re:ceipt</h1>
+      </div>
       <div className="mx-auto w-full max-w-sm">
-        <p className="text-center text-xl font-medium text-white">성함을 입력해주세요.</p>
+        <p className="text-center text-xl font-medium">성함을 입력해주세요.</p>
         <div className="space-y-2">
-          <Input onChange={(e) => setName(e.target.value)} placeholder="홍길동" />
-          <Button onClick={handleConfirm} full aria-label="Confirm">
+          <div>
+            <Input onChange={(e) => setName(e.target.value)} placeholder="홍길동" error={error} />
+            {error && <p className="text-system-error">Your name should be less than 5 letters.</p>}
+          </div>
+          <Button
+            onClick={handleConfirm}
+            full
+            aria-label="Confirm"
+            disabled={name.length == 0 || error}
+          >
             확인
           </Button>
         </div>
