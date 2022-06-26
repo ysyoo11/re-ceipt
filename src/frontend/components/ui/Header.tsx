3aa7fd89075f0ede3943ea0ec547ca9cf5c53296
Yeonsuk Yoo <yysuk0302@gmail.com>
2022-06-26 10:14:50 -0400

    style: Minor css edit on header

--- src/frontend/components/ui/Header.tsx
+++ src/frontend/components/ui/Header.tsx
@@ -14,8 +14,8 @@ export default function Header() {
       <header className="sticky top-0 z-[1] bg-white px-4 py-2 shadow-md">
         <div className="mx-auto flex w-full max-w-sm items-center justify-between">
           <h1 className="text-xl font-semibold text-gray-800">Re:ceipt</h1>
-          <div className="flex items-center space-x-1.5 text-gray-700">
-            <span>{user}</span>
+          <div className="flex items-center space-x-1.5">
+            <span className="font-medium text-gray-800">{user}</span>
             <button onClick={() => setShowModal(true)} aria-label="Edit name">
               <PencilIcon className="h-4 w-4 text-system-link" />
             </button>
